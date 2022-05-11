e655fec8ff0d9fcda6f0f7770b05d9fb328f3cd0
Mr-DecodeBlock <decodeblock1010@gmail.com>
2022-05-11 18:07:32 -0500

    main

--- src/components/Chains/Chains.jsx
+++ src/components/Chains/Chains.jsx
@@ -22,11 +22,11 @@ const styles = {
 };
 
 const menuItems = [
-  {
-    key: "0x1",
-    value: "Ethereum",
-    icon: <ETHLogo />,
-  },
+  // {
+  //  key: "0x1",
+  //  value: "Ethereum",
+  //  icon: <ETHLogo />,
+  // },
   // {
   //   key: "0x539",
   //   value: "Local Chain",
@@ -57,21 +57,21 @@ const menuItems = [
   //   value: "Binance",
   //   icon: <BSCLogo />,
   // },
-  // {
-  //   key: "0x61",
-  //   value: "Smart Chain Testnet",
-  //   icon: <BSCLogo />,
-  // },
+   {
+     key: "0x61",
+     value: "Smart Chain Testnet",
+     icon: <BSCLogo />,
+   },
   // {
   //   key: "0x89",
   //   value: "Polygon",
   //   icon: <PolygonLogo />,
   // },
-  {
-    key: "0x13881",
-    value: "Mumbai",
-    icon: <PolygonLogo />,
-  },
+  // {
+  //  key: "0x13881",
+  //  value: "Mumbai",
+  //  icon: <PolygonLogo />,
+  // },
   // {
   //   key: "0xa86a",
   //   value: "Avalanche",
